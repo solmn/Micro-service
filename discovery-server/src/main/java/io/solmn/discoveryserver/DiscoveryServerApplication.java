66faadcf5399a3e31df5d984acb6f4f193b2d508
Solmn <solomonmolla10@gmail.com>
2020-03-21 08:46:53 -0500

    Added eureka service discovery.

---
+++ discovery-server/src/main/java/io/solmn/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package io.solmn.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
